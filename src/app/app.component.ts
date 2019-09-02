67fc3cc993c9c0b77b6eed60937077d527e81b0b
SarvM <saravana.msi@outlook.com>
2019-09-02 23:30:40 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   name = 'Angular';
 }
+
+
+
+/**
+ * In the @Injectable() decorator for the service itself. 
+ * In the @NgModule() decorator for an NgModule.
+ * In the @Component() decorator for a component.
+ */
